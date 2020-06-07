052db1f0fa8429a5b32b0e3b4e2bdd7ae5f92e19
linksta7 <linksta7@gmail.com>
2020-06-07 16:39:22 -0400

    implemented spawner

---
+++ Assets/Scripts/platformMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformMovement : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rb.transform.position.x = rb.transform.position.x;
+
+		rb.MovePosition (rb.position + Vector2.left * Time.fixedDeltaTime * speed);
+	}
+}
---
+++ Assets/Scripts/platformSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformSpawner : MonoBehaviour {
+
+	public GameObject platformPrefab;
+
+	// the time that it will spawn
+	public float spawnTime = 2f;
+	// interval of time between spawns
+	public float spawnInterval = 1f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		// platforms spawn when whenever new interval is hit
+		if (Time.time >= spawnTime) {
+			SpawnPlatforms ();
+			spawnTime = Time.time + spawnInterval;
+		}
+	}
+
+	void SpawnPlatforms () {
+		Instantiate (platformPrefab, gameObject.transform.position, Quaternion.identity);
+	}
+}

75bf91dc5f24ac08673dcf0ce5c2db7ce552e9d5
linksta7 <linksta7@gmail.com>
2020-06-08 21:24:29 -0400

    implemented basic menu

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour {
+
+	public GameObject mainMenuUI;
+	public GameObject instructionsMenuUI;
+
+	// Main Menu button functions
+	public void hitPlay () {
+		mainMenuUI.SetActive (false);
+	}
+
+	public void hitInstructions () {
+		mainMenuUI.SetActive (false);
+		instructionsMenuUI.SetActive (true);
+	}
+		
+	public void hitQuit () {
+		Application.Quit();
+	}
+
+	// Instruction Menu button functions
+	public void hitMenu () {
+		instructionsMenuUI.SetActive (false);
+		mainMenuUI.SetActive (true);
+	}
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,26 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	private Vector2 pos;
 	private bool touchingPlatform = false;
 	public float jumpVelocity = 5f;
 
-	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();	
 	}
-	
-	// Update is called once per frame
+
 	void FixedUpdate () {
+		// jumping functoin (only jumps when touching platform)
 		if (Input.GetKeyDown (KeyCode.Space) && touchingPlatform == true) {
 			rb.velocity = Vector2.up * jumpVelocity;
 			touchingPlatform = false;
 		}
+
+		// fixes x position of player unless below platform level
+		pos = transform.position;
+		if (transform.position.y >= -4) {
+			// won't let me modify transform.position directly
+			pos.x = -2;
+			transform.position = pos;
+		}
 	}
 
+	// checks if collided (touching) platform
 	void OnCollisionEnter2D(Collision2D collidedObject) {
 		if (collidedObject.gameObject.tag == "platform") {
 			touchingPlatform = true;

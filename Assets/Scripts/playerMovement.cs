fef10a7b30e62ea8da733c52d061157a88139cf0
linksta7 <linksta7@gmail.com>
2020-07-04 21:37:54 -0400

    Initial jumping anim set up

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,21 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour {
 
+	// GameObject variables
 	private Rigidbody2D rb;
+	public GameObject loserMenuUI;
+	public Animator animator;
+
+	// Script variables
 	private Vector2 pos;
 	private bool touchingPlatform = false;
 	public float jumpVelocity = 5f;
-	public GameObject loserMenuUI;
 
 	void Start () {
+		// Gets native GameObject's rigidbody
 		rb = GetComponent<Rigidbody2D> ();	
+		// On entry initiate playerJump animation to false be default
+		animator.SetBool ("playerJump", false);
 	}
 
 	void FixedUpdate () {
 		// jumping functoin (only jumps when touching platform)
 		if (Input.GetKeyDown (KeyCode.Space) && touchingPlatform == true) {
 			rb.velocity = Vector2.up * jumpVelocity;
+			// touchingPlatform to false to disable multi-jump
 			touchingPlatform = false;
+			animator.SetBool ("playerJump", true);
 		}
 
 		// fixes x position of player unless below platform level
@@ -42,6 +51,7 @@ public class playerMovement : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D collidedObject) {
 		if (collidedObject.gameObject.tag == "platform") {
 			touchingPlatform = true;
+			animator.SetBool ("playerJump", false);
 		}
 	}
 }

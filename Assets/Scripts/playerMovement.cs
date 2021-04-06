d22e3bac52919ee71178a7721998bf5138cc22d1
ericchowder <linksta7@gmail.com>
2021-04-06 17:03:02 -0400

    Player movement script cleanup

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -17,6 +17,7 @@ public class playerMovement : MonoBehaviour {
 	//private bool touchingPlatform = false;
 	public float jumpVelocity = 5f;
 
+
 	void Start() {
 		// Gets native GameObject's rigidbody
 		rb = GetComponent<Rigidbody2D>();	
@@ -26,17 +27,8 @@ public class playerMovement : MonoBehaviour {
 		animator.SetBool("playerJump", false);
 	}
 
-	void Update() {
-		// jumping function (only jumps when touching platform)
-		/* old jump function
-		if (Input.GetKeyDown (KeyCode.Space) && touchingPlatform == true) {
-			rb.velocity = Vector2.up * jumpVelocity;
-			// touchingPlatform to false to disable multi-jump
-			touchingPlatform = false;
-			animator.SetBool ("playerJump", true);
-		}
-		*/
 
+	void Update() {
 		// new jump function
 		if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) {
 			rb.velocity = Vector2.up * jumpVelocity;
@@ -56,21 +48,9 @@ public class playerMovement : MonoBehaviour {
 		if (transform.position.y < -6) {
 			loserMenuUI.SetActive(true);
 		}
-
-		//Debug.Log ("Current Time is: " + Time.time);
-		//Debug.Log ("is grounded: " + isGrounded());
 	}
 
-	// checks if collided (touching) platform
-	/*
-	void OnCollisionEnter2D(Collision2D collidedObject) {
-		if (collidedObject.gameObject.tag == "platform") {
-			touchingPlatform = true;
-			animator.SetBool ("playerJump", false);
-		}
-	}
-	*/
-
+	// Disable jump animation when player touching platform
 	void OnCollisionEnter2D(Collision2D collidedObject) {
 		if (collidedObject.gameObject.tag == "platform") {
 			animator.SetBool("playerJump", false);
@@ -78,6 +58,7 @@ public class playerMovement : MonoBehaviour {
 	}
 
 
+	// Check to see if player is grounded with raycast
 	bool isGrounded() {
 		// Boxcast(origin, size, angle, direction, distance)
 		RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, 
